25d09d1b6c562311ffb4a86381786c30a4d645e2
Yusuf <y.abdulwasiu@gmail.com>
2024-02-11 19:58:31 +0100

    feat: :sparkles: short hand method for objet

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-expressions */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  console.log(budget);
+
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
