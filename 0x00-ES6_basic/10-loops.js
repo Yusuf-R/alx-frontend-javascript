0eeaf3afa2880609c0f4af0d8b7640d4d84fbd5b
Yusuf <y.abdulwasiu@gmail.com>
2024-02-12 17:20:23 +0100

    feat: :sparkles: using for..in to an Array object

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+  return array;
+}
