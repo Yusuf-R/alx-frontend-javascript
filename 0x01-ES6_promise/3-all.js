2730742e97e2972a2093fff6b0cd7b7513adf411
Yusuf <y.abdulwasiu@gmail.com>
2024-02-13 02:39:51 +0100

    feat: :sparkles: resolving all promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [uploadPhotoResult, createUserResult] = results;
+      const { body } = uploadPhotoResult;
+      const { firstName, lastName } = createUserResult;
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+export default handleProfileSignup;
