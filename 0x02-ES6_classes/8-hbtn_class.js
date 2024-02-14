e16649e24a76ced78d065109cc74d8daf6eb25a0
Yusuf <y.abdulwasiu@gmail.com>
2024-02-14 09:56:22 +0100

    feat: :sparkles: using Symbol.toPrimitive for typecasiting and coercion

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-dupe-class-members */
+/* eslint-disable no-underscore-dangle */
+
+class HbtnClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
+
+export default HbtnClass;
