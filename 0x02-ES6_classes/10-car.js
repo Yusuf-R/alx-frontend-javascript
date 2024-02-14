fd41829fb805092b39228b0c773bd7ae3d916fcc
Yusuf <y.abdulwasiu@gmail.com>
2024-02-14 15:48:52 +0100

    feat: :sparkles: use symbol.species

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,26 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 class Car {
   constructor(brand, motor, color) {
-    // if (typeof brand !== 'string') {
-    //   throw new TypeError('Brand must be a string');
-    // }
-    // if (typeof motor !== 'string') {
-    //   throw new TypeError('Motor must be a string');
-    // }
-    // if (typeof color !== 'string') {
-    //   throw new TypeError('Color must be a string');
-    // }
-
-    // no need for the error handling because we intend to implement shallow copy
-    // with the cloneCar method
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new this.constructor();
+    const NewModel = this.constructor[Symbol.species];
+    return new NewModel();
   }
 }
 
