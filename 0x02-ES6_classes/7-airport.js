69fa289f216983915a0389ad0fe77b013e4767ca
Yusuf <y.abdulwasiu@gmail.com>
2024-02-14 08:56:43 +0100

    feat: :sparkles: using Sybmbols for class object defination

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-dupe-class-members */
+/* eslint-disable no-underscore-dangle */
+class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  [Symbol.toStringTag]() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
+
+export default Airport;
