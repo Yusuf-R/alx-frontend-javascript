434acefe8d125d169b5dcfa4f64b6a55fd031459
Yusuf <y.abdulwasiu@gmail.com>
2024-02-14 04:19:31 +0100

    fix: :bug: fixed the issue from inheritance

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,25 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
 class Building {
-  constructor(sqrt) {
-    if (typeof sqrt !== 'number') {
-      return ('sqrt must be a number');
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      return ('sqft must be a number');
     }
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqrt = sqrt;
+    this._sqft = sqft;
   }
 
-  get sqrt() {
-    return this._sqrt;
+  get sqft() {
+    return this._sqft;
   }
 
-  set sqrt(data) {
+  set sqft(data) {
     if (typeof data !== 'number') {
       throw new TypeError('data must be a number');
     }
-    this._amount = data;
+    this._sqft = data;
   }
 }
 
