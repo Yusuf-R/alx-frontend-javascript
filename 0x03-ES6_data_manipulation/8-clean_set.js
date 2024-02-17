dffeaf2ff43fd609e6f29059e685cd77b9ac099c
Yusuf <y.abdulwasiu@gmail.com>
2024-02-17 21:57:24 +0100

    fix: :bug: parameter validation before functin processing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 function cleanSet(setData, startString) {
+  if (!(setData instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
   let newStr = '';
   if (startString.length === 0) {
     return newStr;
