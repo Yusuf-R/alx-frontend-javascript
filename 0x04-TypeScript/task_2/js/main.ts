c931786cdd23c962147c7fcdbacab9253432f781
Yusuf <y.abdulwasiu@gmail.com>
2024-02-18 22:42:55 +0100

    feat: :sparkles: using typed interface

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -68,6 +68,35 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+// task 6
+function isDirector(employee: Director | Teacher ): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;    
+}
+
+function executeWork(employee: Director | Teacher) {
+  if ((employee as Director).workDirectorTasks) {
+      return (employee as Director).workDirectorTasks()
+  }
+  return (employee as Teacher).workTeacherTasks()    
+}
+
+// Define the string literal type
+type Subjects = "Math" | "History";
+
+// Define the function with type annotation for todayClass parameter
+function teachClass(todayClass: Subjects): string {
+    // Use a switch statement to handle the different values of todayClass
+    switch (todayClass) {
+        case "Math":
+            return 'Teaching Math';
+        case "History":
+            return 'Teaching History';
+        default:
+            // Handle unexpected values gracefully
+            return 'Unknown class';
+    }
+}
+
 
 const stdA = new StudentClass('Yusuf', 'Abdulwasiu');
 console.log(stdA.workOnHomework());
@@ -76,3 +105,6 @@ console.log(stdA.displayName());
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
